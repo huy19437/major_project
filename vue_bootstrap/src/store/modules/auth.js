de210d9753b322c870c6fb32c012c419155f4284
Nguyen Huy <huy19437@users.noreply.github.com>
2021-02-28 16:09:53 +0700

    To do app

---
+++ vue_bootstrap/src/store/modules/auth.js
@@ -0,0 +1,18 @@
+const authModule = {
+	state : {
+		auth: {
+			isAuthenticated: false
+		}
+	},
+	getters :{
+		isAuthenticated: state => state.auth.isAuthenticated
+	},
+	actions : {},
+	mutations : {
+		TOGGLE_AUTH(state) {
+			state.auth.isAuthenticated = !state.auth.isAuthenticated
+		}
+	}
+}
+
+export default authModule
