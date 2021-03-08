38984ae9bdedaf8851b83602b36c40f76ffb5160
Nguyen Huy <huy19437@users.noreply.github.com>
2021-03-08 23:05:57 +0700

    change name component to view products

--- qakha_v1/src/components/homepage/content/Content.vue
+++ qakha_v1/src/components/homepage/content/Content.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <Container />
+    <ListProducts />
     <Carousel />
   </div>
 </template>
 
 <script>
-import Container from "@/components/homepage/content/container/Container";
+// import Container from "@/components/homepage/content/container/Container";
+import ListProducts from "@/components/homepage/content/listProducts/ListProducts";
 import Carousel from "@/components/homepage/content/carousel/Carousel";
 export default {
   components: {
+    ListProducts,
     Carousel,
-    Container,
   },
   data() {
     return {};
