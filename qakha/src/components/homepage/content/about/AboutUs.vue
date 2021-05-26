b89e92963c52b00e04d31a983b2c08cd053ee82c
Nguyen Huy <huy19437@users.noreply.github.com>
2021-05-27 01:10:19 +0700

    fix about ú

--- qakha/src/components/homepage/content/about/AboutUs.vue
+++ qakha/src/components/homepage/content/about/AboutUs.vue
@@ -7,6 +7,18 @@
           <div class="col-lg-4">
             <div class="testimonial-item mx-auto mb-5 mb-lg-0">
               <img
+                src="./thay_thi.jpg"
+                alt=""
+                class="img-fluid rounded-circle mb-3"
+              />
+              <h5>Trịnh Sử Trường Thi</h5>
+              <h4>Mentor</h4>
+              <p class="font-weight-light mb-0">"Be all you can be."</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+              <img
                 src="./huy2.jpg"
                 alt=""
                 class="img-fluid rounded-circle mb-3"
@@ -49,38 +61,38 @@
               </p>
             </div>
           </div>
-          <div class="col-lg-12 flex-center">
-            <div class="col-lg-4">
-              <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                <img
-                  src="./an.jpg"
-                  alt=""
-                  class="img-fluid rounded-circle mb-3"
-                />
-                <h5>Võ Văn An</h5>
-                <h4>Mobile</h4>
-                <p class="font-weight-light mb-0">
-                  "It’s better to cross the line and suffer the consequences
-                  than to just stare at the line for the rest of your life."
-                </p>
-              </div>
-            </div>
-            <div class="col-lg-4">
-              <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                <img
-                  src="./khang.png"
-                  alt=""
-                  class="img-fluid rounded-circle mb-3"
-                />
-                <h5>Võ Văn Khang</h5>
-                <h4>Mobile</h4>
-                <p class="font-weight-light mb-0">
-                  "Life is not about waitng for the storm to pass, it’s about
-                  learning to dance in the rain."
-                </p>
-              </div>
+          <!-- <div class="col-lg-12 flex-center"> -->
+          <div class="col-lg-4">
+            <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+              <img
+                src="./an.jpg"
+                alt=""
+                class="img-fluid rounded-circle mb-3"
+              />
+              <h5>Võ Văn An</h5>
+              <h4>Mobile</h4>
+              <p class="font-weight-light mb-0">
+                "It’s better to cross the line and suffer the consequences than
+                to just stare at the line for the rest of your life."
+              </p>
             </div>
           </div>
+          <div class="col-lg-4">
+            <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+              <img
+                src="./khang.png"
+                alt=""
+                class="img-fluid rounded-circle mb-3"
+              />
+              <h5>Võ Văn Khang</h5>
+              <h4>Mobile</h4>
+              <p class="font-weight-light mb-0">
+                "Life is not about waitng for the storm to pass, it’s about
+                learning to dance in the rain."
+              </p>
+            </div>
+          </div>
+          <!-- </div> -->
         </div>
       </div>
     </section>
@@ -113,7 +125,40 @@ export default {};
     .img-fluid {
       width: 140px !important;
       height: 140px !important;
+      -webkit-animation-name: opacity;
+      animation-name: opacity;
+      -webkit-animation-duration: 1s;
+      animation-duration: 1s;
+      border: 3px solid #ccc;
+      &:hover {
+        border: 3px solid #f6931d;
+      }
     }
+  }
+}
+@-webkit-keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+    transform: scale(3);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+}
+
+@keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+    transform: scale(3);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
   }
 }
 </style>
